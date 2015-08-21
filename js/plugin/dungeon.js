cf12d75f4468089c10b31f3bee36d07f9b2381cc
irpx <irpx@sci.fi>
2015-08-21 14:22:40 +0300

    dungeon update.

--- js/plugin/dungeon.js
+++ js/plugin/dungeon.js
@@ -60,112 +60,147 @@ function Entity()
 
 function LevelGridSystem()
 {
-	this.grid = [];
+	this.grid = null;
 	this.gridSize = [0,0];
 	this.gridWidth = 100;
 	this.gridHeight = 75;
 	
 }{
-	LevelGridSystem.prototype.setGrid=function(level)
+	LevelGridSystem.prototype.setGridSize=function(w,h)
 	{
-		this.grid = level;
-		this.gridSize = [this.grid.length,this.grid[0].length];
+		this.gridSize = [w,h];
 	}
 	
 	LevelGridSystem.prototype.setEntity=function(x,y,entity)
 	{
+		if(this.grid==null)
+		{
+			this.grid=[];
+		}
+		
+		if(this.grid[x]==null)
+		{
+			this.grid[x]=[];
+		}
+
 		if(this.grid[x][y]!=null)
 		{
-			this.grid[x][y].addComponent({name:"entityInvade",entity:entity});
+			log("Entity action!");
+			this.grid[x][y].addComponent({name:"entityAction",entity:entity});
 		}
 		this.grid[x][y] = entity;
 	}
 	
 	LevelGridSystem.prototype.update=function(entities)
 	{
+		//Clear grid records.
+		
+		this.grid=null;
 		for	(var i = 0 ; i<entities.length;i++)
 		{
-			var components = entities[i].components;
+			var entity = entities[i];
+			var components = entity.components;
 			var position = components.position;
 			
 			if(position==null) continue;
 			
 			if(components.camera) continue;
 			
-			//entities[i].addComponent({name:"debugText",text:"Grid:"+position.x+", "+position.y});
+			//entity.addComponent({name:"debugText",text:"Grid:"+position.x+", "+position.y});
 			
 			var renderPosition = components.renderPosition;
 			if(renderPosition == null)
 			{
 				renderPosition = {name:"renderPosition",x:0,y:0};
-				entities[i].addComponent(renderPosition);
+				entity.addComponent(renderPosition);
 			}
 			
 			var move = components.move;
+			var movement = components.movement;
+			
+			//Get movedata into movement component.
 			if(move)
 			{
-				if(move.gridChecked!=true)
+				if(!movement)
 				{
-					move.gridChecked=true;
-
-					move.x += position.x;
-					move.y += position.y;
-					
-					move.currentX = position.x*this.gridWidth;
-					move.currentY = position.y*this.gridHeight;
-					move.targetX = move.x*this.gridWidth;
-					move.targetY = move.y*this.gridHeight;
-					move.speed = move.speed || 4;
+					movement = {name:"movement"};
+					entity.addComponent(movement);
 				}
 				
+				//set movement target & values.
+				//Target Grid
+				movement.x = position.x+move.x;
+				movement.y = position.y+move.y;
+				//Target render position
+				movement.targetX = movement.x*this.gridWidth;
+				movement.targetY = movement.y*this.gridHeight;
+				movement.speed = move.speed || 4;
+				//Update or set current render position
+				movement.currentX = movement.currentX || position.x*this.gridWidth;
+				movement.currentY = movement.currentY || position.y*this.gridHeight;
+				
+				//Remove move data
+				entity.removeComponent(move.name);
+				move = null;
+			}
+			
+			if(movement)
+			{
 				var finished = true;
 				
-				var dif = move.targetX-move.currentX;
+				var dif = movement.targetX-movement.currentX;
 				
 				if (dif>0)
 				{
-					move.currentX += Math.min(dif,move.speed);
+					movement.currentX += Math.min(dif,movement.speed);
 					finished = false;
 				}
 				else if (dif<0)
 				{
-					move.currentX -= Math.min(-dif,move.speed);
+					movement.currentX -= Math.min(-dif,movement.speed);
 					finished = false;
 				}
 				
-				dif = move.targetY - move.currentY;
+				dif = movement.targetY - movement.currentY;
 				if (dif>0)
 				{
-					move.currentY += Math.min(dif,move.speed);
+					movement.currentY += Math.min(dif,movement.speed);
 					finished = false;
 				}
 				else if (dif<0)
 				{
-					move.currentY -= Math.min(-dif,move.speed);
+					movement.currentY -= Math.min(-dif,movement.speed);
 					finished = false;
 				}
 				
 				if(finished)
 				{
-					position.x = move.x;
-					position.y = move.y;
+					position.x = movement.x;
+					position.y = movement.y;
 					
 					renderPosition.x = position.x*this.gridWidth;
 					renderPosition.y = position.y*this.gridHeight;	
 					
-					entities[i].removeComponent(move.name);
-					move=null;
+					entity.removeComponent(movement.name);
+					movement=null;
+					
+					
 				}
 				else
 				{
-					renderPosition.x = move.currentX;
-					renderPosition.y = move.currentY;
+					renderPosition.x = movement.currentX;
+					renderPosition.y = movement.currentY;
 				}
 			}
 			else
 			{
 				renderPosition.x = position.x*this.gridWidth;
 				renderPosition.y = position.y*this.gridHeight;	
+			}
+			
+			if(components.gridObject)
+			{
+				this.setEntity(position.x,position.y,entity);
 			}
 		}
 	}
@@ -185,7 +220,7 @@ function RenderSystem(ctx)
 	this.context=ctx;
 	this.images = {};
 	
-	this.offset = [100,100];
+	this.offset = [200,200];
 	
 	this.imageMapping = {
 		characterSmall:"js/plugin/dungeon/character1.png",
@@ -195,7 +230,10 @@ function RenderSystem(ctx)
 		dirt:"js/plugin/dungeon/Dirt Block.png",
 		stone:"js/plugin/dungeon/Stone Block.png",
 		chest:"js/plugin/dungeon/Chest Closed.png",
-		chestOpen:"js/plugin/dungeon/Chest Open.png"
+		chestOpen:"js/plugin/dungeon/Chest Open.png",
+		selector:"js/plugin/dungeon/Selector.png",
+		rock:"js/plugin/dungeon/Rock.png",
+		heart:"js/plugin/dungeon/Heart.png"
 	};
 }{
 	RenderSystem.prototype.drawText=function(ctx,text,x,y,size,align)
@@ -203,11 +241,23 @@ function RenderSystem(ctx)
 		drawText(ctx,text,x+this.offset[0], y+this.offset[1],size,align);
 	}
 	
+	RenderSystem.prototype.drawImage = function(img,x,y,w,h)
+	{
+		this.context.drawImage(
+			img,
+			x+this.offset[0]-w/2,
+			y+this.offset[1]-h/2,
+			w,
+			h
+		);
+	}
+	
 	RenderSystem.prototype.update=function(entities)
 	{
 		for	(var i = 0 ; i<entities.length;i++)
 		{
-			var components = entities[i].components;
+			var entity = entities[i];
+			var components = entity.components;
 			
 			var position = components.renderPosition;
 			var image = components.image;
@@ -222,7 +272,7 @@ function RenderSystem(ctx)
 				this.offset[1] += components.position.y;
 				components.position.x = 0;
 				components.position.y = 0;
-				//entities[i].removeComponent(position.name);
+				//entity.removeComponent(position.name);
 				continue;
 			}
 			
@@ -230,18 +280,33 @@ function RenderSystem(ctx)
 			
 			if(image)
 			{
-				
 				var img = this.getImage(image.source);
 				if(img!=null)
 				{
-					this.context.drawImage(
+					this.drawImage(
 						img,
-						position.x-img.width/2+this.offset[0],
-						position.y-img.height/2+this.offset[1],
+						position.x,
+						position.y,
 						img.width,
 						img.height
 					);
 				}
+			}
+			
+			if(components.entityAction)
+			{
+				var img = this.getImage("selector");
+				if(img!=null)
+				{
+					this.drawImage(
+						img,
+						position.x,
+						position.y,
+						img.width,
+						img.height
+					);
+				}
+				entity.removeComponent(image.name);
 			}
 			
 			if(text)
@@ -257,7 +322,7 @@ function RenderSystem(ctx)
 				}
 				else
 				{
-					entities[i].removeComponent(text.name);
+					entity.removeComponent(text.name);
 					text=null;
 				}
 			}
@@ -299,7 +364,8 @@ function Dungeon(pluginContext)
 	this.updateRate = 16;
 	this.camera;
 	
-	this.titleText = "Welcome to "+this.pluginContext.channel+" Dungeon!";
+	this.log = ["Welcome to "+this.pluginContext.channel+" Dungeon!"];
+	this.logLimit = 50;
 	
 	this.init();
 	
@@ -313,7 +379,12 @@ function Dungeon(pluginContext)
 		
 		this.engine.update();
 		//Title text
-		drawText(this.context,this.titleText,10,10,24,"left");
+		var y = 10;
+		for	(var i = 0; i<this.log.length; i++)
+		{
+			drawText(this.context,unwindHtml(this.log[i]),10,y,10,"left");
+			y += 10;
+		}
 	}
 
 	Dungeon.prototype.init = function()
@@ -451,7 +522,8 @@ function Dungeon(pluginContext)
 		//Send initialization to chat
 		this.pluginContext.addLine("+position"+JSON.stringify({x:2,y:2}),this.pluginContext.getChannelRealName());
 		this.pluginContext.addLine("+image"+JSON.stringify({source:"character"}),this.pluginContext.getChannelRealName());
-			
+		this.pluginContext.addLine("+gridObject{}",this.pluginContext.getChannelRealName());
+		
 		//entity.addComponent({name:"position",x:100,y:100});
 		//entity.addComponent({name:"nameLabel",text:name});
 		//entity.addComponent({name:"image",source:"character"});
@@ -480,10 +552,15 @@ function Dungeon(pluginContext)
 
 	Dungeon.prototype.onChat = function(time,who,what,marker,channel)
 	{
-		if(who=="SYSTEM" || marker == true)
+		if(channel != this.pluginContext.getChannelRealName())
 		{
-			this.titleText = who+": "+what;
 			return;
+		}
+		
+		this.log.push(who+": "+what);
+		while(this.log.length>this.logLimit)
+		{
+			this.log.shift();
 		}
 		
 		//log("Dungeon Chat: "+time+who+what+marker+channel);
@@ -520,34 +597,40 @@ function Dungeon(pluginContext)
 	Dungeon.prototype.createLevel=function()
 	{
 		var size = [7,4];
-		
-		var levelGrid = [];
+		this.level.setGridSize(size[0],size[1]);
 		
 		for(var i = 0; i<size[0]; i++)
 		{
-			var ar = [];
 			for(var j = 0; j<size[1]; j++)
 			{
-				ar.push(null);
-				var entity = new Entity();
-				entity.addComponent({name:"position",x:i,y:j});
-				entity.addComponent({name:"image",source:"dirt"});
-				
-				this.engine.addEntity(entity);
+				this.engine.addEntity(this.createBackgroundBlock(i,j));
 			}
-			levelGrid.push(ar);
 		}
-		this.level.setGrid(levelGrid);
-		this.createChest(3,6);
+
+		this.engine.addEntity(this.createObject("chest",1,1));
+		this.engine.addEntity(this.createObject("rock",3,1));
+		this.engine.addEntity(this.createObject("rock",3,4));
+		this.engine.addEntity(this.createObject("rock",5,2));
+		this.engine.addEntity(this.createObject("stone",4,2));
+		this.engine.addEntity(this.createObject("heart",1,0));
 	}
 	
-	Dungeon.prototype.createChest=function(x,y)
+	Dungeon.prototype.createBackgroundBlock = function(x,y)
 	{
 		var entity = new Entity();
-		entity.addComponent({name:"position",x:x*this.blockSizeX,y:y*this.blockSizeY});
-		entity.addComponent({name:"image",source:"chest"});
-		this.engine.addEntity(entity);
-		this.level.setEntity(x,y,entity);
+		entity.addComponent({name:"position",x:x,y:y});
+		entity.addComponent({name:"image",source:"dirt"});
+		return entity;
+	}
+	
+	Dungeon.prototype.createObject=function(name,x,y)
+	{
+		var entity = new Entity();
+		entity.addComponent({name:name});
+		entity.addComponent({name:"gridObject"});
+		entity.addComponent({name:"position",x:x,y:y});
+		entity.addComponent({name:"image",source:name});
+		return entity;
 	}
 
 	Dungeon.prototype.moveLeft=function()
@@ -574,342 +657,3 @@ function Dungeon(pluginContext)
 		log("Dungeon destroy! "+this);
 	}
 }
-
-/*
-var clickX = new Array();
-var clickY = new Array();
-var clickDrag = new Array();
-var paint;
-var context;
-var pluginContext;
-var userColor = "#000000";
-var userSize = 2;
-var container;
-var userImage;
-var chatMsg = "";
-
-function initValues()
-{
-	clickX = new Array();
-	clickY = new Array();
-	clickDrag = new Array();
-	paint = false;
-	context = null;
-	pluginContext = null;
-	userColor = "#000000";
-	userSize = 2;
-	container = null;
-	userImage = null;
-	chatMsg = "";
-}
-
-function addClick(x, y, dragging)
-{
-  clickX.push(x);
-  clickY.push(y);
-  clickDrag.push(dragging);
-  
-  if(dragging!=true)
-  {	
-	chatMsg = userColor+","+userSize+";";
-  }
-  else
-  {
-	chatMsg += x+","+y+";";
-  }
-}
-
-function onStopDraw()
-{
-	paint=false;
-	if(chatMsg.length>3)
-	{
-		pluginContext.addLine(chatMsg,pluginContext.channel+"#whiteboard");
-	}
-	chatMsg="";
-	
-	//context.closePath();
-	context.stroke();
-}
-
-function clear()
-{
-	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-}
-
-function onStartDraw(x,y)
-{
-	context.strokeStyle = userColor;
-	context.lineJoin = "round";
-	context.lineWidth = userSize;
-		
-	context.beginPath();
-	context.moveTo(x, y-1);
-	context.lineTo(x, y);
-}
-
-function onDraw(x,y)
-{
-	context.lineTo(x, y);
-	context.stroke();
-}
-
-function draw(style,join,width,data)
-{
-	if(context == null)
-	{
-		//TODO push cache & restore later
-		return;
-	}
-	
-	context.strokeStyle = style || "#000000";
-	context.lineJoin = join || "round";
-	context.lineWidth = width || 2;
-		
-	context.beginPath();
-	for(var i=0; i < data.length; i++)
-	{		
-		if(i==0)
-		{
-			context.moveTo(data[i][0]-1, data[i][1]);
-		}
-		context.lineTo(data[i][0], data[i][1]);
-	}
-	//context.closePath();
-	context.stroke();
-}
-
-function placeImage(x,y)
-{
-	if(userImage!=null)
-	{
-		client.socket.emit('chat message', pluginContext.channel+"#whiteboard" + "|" + "#image,"+x+","+y+";"+userImage);
-	}
-	userImage=null;
-}
-
-function drawImage(src,x,y,w,h)
-{
-	var outlineImage = new Image();
-	outlineImage.src = src;
-	w = w || outlineImage.width;
-	h = h || outlineImage.height;
-	context.drawImage(outlineImage, x||0, y||0, w, h);
-}
-
-dungeonChatPluginInit = function(pcontext)
-{
-	pluginContext = pcontext;
-	log("WB init! context:"+pluginContext);
-	
-	var whiteboardID = "#whiteboard";
-	var canvas;
-	
-	if(context!=null)
-	{
-		canvas = document.getElementById(whiteboardID.substring(1));
-		$(container).show();
-	}
-	else
-	{
-		var h = $(window).height();
-		var w = $(window).width();
-		var headerSize = 80;
-		var usersPanelSize = 155;
-		var wbWidth = w-usersPanelSize;
-		var wbHeight = h-headerSize;
-		
-		container = document.createElement("div");
-		container.id = "whiteboard-container";
-		container.style.display = "block";
-		container.style.width = wbWidth;
-		container.style.height = wbHeight;
-		container.style.position = "fixed";
-		container.style.top = headerSize+"px";
-		container.style.left = usersPanelSize+"px";
-		container.style.borderLeft = "5px solid #000";
-		container.style.background = "#fff";
-		container.style.zIndex = 2;
-		
-		
-		var tools = document.createElement("div");
-		tools.style.border = "1px solid #eee";
-		tools.style.background = "#eee";
-		tools.style.display = "block";
-		tools.style.width = "100%";
-		
-		var addLabel = function(label)
-		{
-			var btn = document.createElement("div");
-			btn.className = "label label-info";
-			btn.innerHTML = label;
-			tools.appendChild(btn);
-		};
-		
-		var addButton = function(btnText)
-		{
-			var btn = document.createElement("button");
-			btn.className = "btn btn-default btn-xs";
-			btn.innerHTML = "<div style='background:"+btnText+";'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>";
-			btn.setAttribute("userColor",btnText);
-			$(btn).click(function()
-			{
-				userColor = this.getAttribute("userColor");
-				log(userColor);
-			});
-			tools.appendChild(btn);
-		};
-		
-		var addSizeButton = function(size)
-		{
-			var btn = document.createElement("button");
-			btn.className = "btn btn-default btn-xs";
-			btn.innerHTML = size;
-			btn.setAttribute("userSize",size);
-			$(btn).click(function()
-			{
-				userSize = this.getAttribute("userSize");
-				log(userSize);
-			});
-			tools.appendChild(btn);
-		};
-		addLabel("Color");
-		addButton("#000000");
-		addButton("#ffffff");
-		addButton("#666666");
-		addButton("#ff0000");
-		addButton("#00ff00");
-		addButton("#0000ff");
-		addButton("#00ffff");
-		addButton("#ffff00");
-		addButton("#ff00ff");
-		
-		addLabel("Size");
-		addSizeButton(1);
-		for(var i = 2; i<=10; i++)
-		{
-			addSizeButton(i*i);
-		}
-		
-		var toolsPanelSize = $(tools).height();
-		
-		canvas = document.createElement("canvas");
-		canvas.id=whiteboardID.substring(1);
-		canvas.setAttribute("width",wbWidth);
-		canvas.setAttribute("height",wbHeight-toolsPanelSize);
-		canvas.style.width = wbWidth;
-		canvas.style.height = wbHeight;
-		canvas.style.cursor = "crosshair";
-		
-		document.body.appendChild(container);
-		container.appendChild(tools);
-		container.appendChild(canvas);
-				
-		context = canvas.getContext("2d");
-	}
-	
-	var offSetX = -usersPanelSize-4;
-	var offSetY = -headerSize-toolsPanelSize-22;
-
-	$(whiteboardID).mousedown(function(e)
-	{
-		if(userImage!=null)
-		{
-			placeImage(e.pageX+offSetX, e.pageY+offSetY);
-			return;
-		}
-		
-		paint = true;
-		addClick(e.pageX+offSetX, e.pageY+offSetY);
-		onStartDraw(e.pageX+offSetX, e.pageY+offSetY);
-	});
-	
-	$(whiteboardID).mousemove(function(e)
-	{
-		if(paint)
-		{
-			addClick(e.pageX+offSetX, e.pageY+offSetY,true);
-			onDraw(e.pageX+offSetX, e.pageY+offSetY);
-		}
-	});
-	
-	$(whiteboardID).mouseup(function(e){onStopDraw();});
-	$(whiteboardID).mouseleave(function(e){onStopDraw();});
-	
-	pluginContext.onClose = function()
-	{
-		paint=false;
-		$(container).hide();
-		$(container).remove();
-		initValues();
-	};
-	pluginContext.onAddLine = function(time, who, what,marker,channel)
-	{
-		if(channel!=pluginContext.channel+"#whiteboard") return false;
-		
-		var data = what.split(";");
-		var output = [];
-		
-		//Verify that we have proper data
-		var type = data.shift().split(",");
-		if(type[0][0]!="#") return false;
-		
-		if(type[0]=="#image")
-		{
-			drawImage(data.join(";"),type[1],type[2]);
-			return;
-		}
-		
-		var color = type[0];
-		var size = type[1];
-		
-		for	(var i =0; i<data.length; i++)
-		{
-			var values = data[i].split(",");
-			output.push([parseInt(values[0]),parseInt(values[1])]);
-		}
-		draw(color,null,size,output);
-		return true;
-	};
-	
-	var history = pluginContext.history;
-	if(history!=null)
-	{
-		for(var index in history)
-		{
-			pluginContext.onAddLine(
-				history[index][0],
-				history[index][1],
-				history[index][2],
-				history[index][3],
-				pluginContext.channel+"#whiteboard"
-			);
-		}
-	}
-	
-	document.onpaste = function(event)
-	{
-		var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-
-		var load = function(idx)
-		{
-			var blob = items[idx].getAsFile();
-			var reader = new FileReader();
-			reader.onload = function(event)
-			{
-				userImage = event.target.result;
-			};
-			reader.readAsDataURL(blob);
-		}
-		
-		try
-		{
-			load(0);
-		}
-		catch(error)
-		{
-			load(1);
-		}
-	}
-}
-*/
