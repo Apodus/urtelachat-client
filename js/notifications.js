eae9dfcbdb298f08478664181a0bab1d3bf3d175
Urtela <apodus@gmail.com>
2015-09-04 12:14:11 +0300

    Remove xml from notifications.

--- js/notifications.js
+++ js/notifications.js
@@ -27,13 +27,17 @@ else
 	}
 }
 
+function stripXml(str) {
+ return str.replace(/(<([^>]+)>)/ig,"");
+}
+
 function showDesktopNotification(channel, user, msg)
 {
 	// if window has no focus & global notify setting & channel specific rules.
 	if((!(channel in notificationsTemporary) || (notificationsTemporary[channel]) == true) && notificationsGlobal && (getCookie(channel + "_notify") != "forbid") && !document.hasFocus())
 	{
 		
-		var notification = new Notification(channel + ": " + user, {"body": unwindHtml(msg), "icon":"http://urtela.redlynx.com/img/chaticon.jpg"});
+		var notification = new Notification(channel + ": " + user, {"body": unwindHtml(stripXml(msg)), "icon":"http://urtela.redlynx.com/img/chaticon.jpg"});
 		setTimeout(function()
 		{
 			notification.close();
